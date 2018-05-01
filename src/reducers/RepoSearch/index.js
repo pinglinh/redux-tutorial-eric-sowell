7f82ba32c0683a76d49a2da7d4003371fc65b278
pinglinh <lnguyenmy1@gmail.com>
2018-05-01 14:35:19 +0100

    Use spread operator for RepoSearch reducer

--- src/reducers/RepoSearch/index.js
+++ src/reducers/RepoSearch/index.js
@@ -5,13 +5,25 @@ const initialState = {
   searchInputValue: ""
 };
 
+// const RepoSearch = (state = initialState, action) => {
+//   console.log("reducer", action);
+//   switch (action.type) {
+//     case SEARCH_INPUT_CHANGE:
+//       return Object.assign({}, state, { searchInputValue: action.value });
+//     case SET_REPOS:
+//       return Object.assign({}, state, { repos: action.repos });
+//     default:
+//       return state;
+//   }
+// };
+
 const RepoSearch = (state = initialState, action) => {
   console.log("reducer", action);
   switch (action.type) {
     case SEARCH_INPUT_CHANGE:
-      return Object.assign({}, state, { searchInputValue: action.value });
+      return { ...state, searchInputValue: action.value };
     case SET_REPOS:
-      return Object.assign({}, state, { repos: action.repos });
+      return { ...state, repos: action.repos };
     default:
       return state;
   }
