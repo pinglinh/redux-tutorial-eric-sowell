3396475be6be14d564c98c2aec193316c63292b9
pinglinh <lnguyenmy1@gmail.com>
2018-05-02 14:25:44 +0100

    Add stateless components folder

--- src/components/RepoSearch/index.js
+++ src/components/RepoSearch/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
-
-import { SEARCH_INPUT_CHANGE } from "../../actions/api";
-import Api from "../../actions/api";
 
 const RepoSearch = props => {
   return (
@@ -24,26 +20,4 @@ const RepoSearch = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    inputValue: state.searchInputValue,
-    repos: state.repos
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    handleInputChange: event => {
-      dispatch({
-        type: SEARCH_INPUT_CHANGE,
-        value: event.target.value
-      });
-    },
-    handleSubmit: (event, query) => {
-      event.preventDefault();
-      Api.getRepos(dispatch, query);
-    }
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(RepoSearch);
+export default RepoSearch;
